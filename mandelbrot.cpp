b2df3d1af2e93b55b88f4993b133658faf2c11ec
Henry J Schmale <hschmale16@gmail.com>
2015-05-07 19:39:34 -0400

    Added Presentation and More Code

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -1,7 +1,13 @@
 #include <cstdio>
 #include <cstdint>
+#include <cstdlib>
 #include <SDL/SDL.h>
 #include <pthread.h>
+
+const uint32_t rmask = 0x000000ff;
+const uint32_t gmask = 0x0000ff00;
+const uint32_t bmask = 0x00ff0000;
+const uint32_t amask = 0xff000000;
 
 const int THREADS  = 4;
 const int SCR_WDTH = 800;
@@ -50,12 +56,18 @@ struct rendThrData{
     double        ymax;
     SDL_Surface*  img;
 
-    rendThrData()
-        :id(next_id++){}
+    rendThrData():id(next_id++){
+        img = SDL_CreateRGBSurface(SDL_SWSURFACE, SCR_WDTH, SCR_WDTH,
+                SCR_CD, rmask, gmask, bmask, amask);
+        if(img == NULL){
+            fprintf(stderr, "Fucked up allocating image in rendthr data\n");
+            exit(0);
+        }
+    }
     ~rendThrData(){
         SDL_FreeSurface(img);
     }
-} thrdata[THREADS];
+};
 uint8_t rendThrData::next_id = 0;
 
 void put_px(SDL_Surface* scr, int x, int y, pixel* p){
@@ -65,16 +77,22 @@ void put_px(SDL_Surface* scr, int x, int y, pixel* p){
             p->alpha);
 }
 
-
+void* renderThread(void *data){
+    rendThrData* d = (rendThrData*)data;
+}
 
 int main(){
-    pthread_t thrds[THREADS];
+    pthread_t    thrds[THREADS];
+    rendThrData* data;
     SDL_Surface* screen;
 
     SDL_Init(SDL_INIT_VIDEO);
     screen = SDL_SetVideoMode(SCR_WDTH, SCR_HGHT, SCR_CD, SDL_SWSURFACE);
+    data = new rendThrData[THREADS];
     for(int i = 0; i < ITERATS; i++){
         SDL_LockSurface(screen);
+        SDL_BlitSurface(data[i % THREADS].img,
+                        NULL, screen, NULL);
         SDL_UnlockSurface(screen);
         SDL_Flip(screen);
     }
---
+++ pres.md
@@ -0,0 +1,5 @@
+---
+title  : The Mandlebrot Set
+author : Henry J Schmale
+date   : May 2015
+---

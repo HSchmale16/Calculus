1f90524daadc22e704b3282f8347ff531b790e52
Henry J Schmale <hschmale16@gmail.com>
2015-05-07 20:41:44 -0400

    Pictures show up now

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -4,16 +4,22 @@
 #include <SDL/SDL.h>
 #include <pthread.h>
 
-const uint32_t rmask = 0x000000ff;
-const uint32_t gmask = 0x0000ff00;
-const uint32_t bmask = 0x00ff0000;
-const uint32_t amask = 0xff000000;
-
 const int THREADS  = 4;
 const int SCR_WDTH = 800;
 const int SCR_HGHT = 600;
-const int SCR_CD   = 24;
+const int SCR_CD   = 32;
 const int ITERATS  = 1000;
+const int MAX_ITER = 512;  //!< Maximum number of iter for mandelbrot
+const int FRAMES   = 100; //!< Frames to render before quiting
+
+/** Maps a value between 2 limits to some other value between 2 other
+ * limits
+ */
+inline double map(double x, double in_min, double in_max, 
+        double out_min, double out_max){
+    return (x - in_min) * (out_max - out_min) / 
+        (in_max - in_min) + out_min;
+}
 
 struct pixel{
     Uint8 r;
@@ -37,10 +43,10 @@ struct pixel{
         }
 };
 
-pixel colorTable[256];
+pixel colorTable[MAX_ITER];
 void generateColorTable(){
     srand(time(0));
-    for(int i = 0; i < 256; i++){
+    for(int i = 0; i < MAX_ITER; i++){
         colorTable[i].r = rand() % 255;
         colorTable[i].g = rand() % 255;
         colorTable[i].b = rand() % 255;
@@ -54,19 +60,10 @@ struct rendThrData{
     double        xmax;
     double        ymin;
     double        ymax;
-    SDL_Surface*  img;
+    uint64_t      img[SCR_WDTH][SCR_HGHT];
 
-    rendThrData():id(next_id++){
-        img = SDL_CreateRGBSurface(SDL_SWSURFACE, SCR_WDTH, SCR_WDTH,
-                SCR_CD, rmask, gmask, bmask, amask);
-        if(img == NULL){
-            fprintf(stderr, "Fucked up allocating image in rendthr data\n");
-            exit(0);
-        }
-    }
-    ~rendThrData(){
-        SDL_FreeSurface(img);
-    }
+    rendThrData():id(next_id++){}
+    ~rendThrData(){}
 };
 uint8_t rendThrData::next_id = 0;
 
@@ -79,22 +76,67 @@ void put_px(SDL_Surface* scr, int x, int y, pixel* p){
 
 void* renderThread(void *data){
     rendThrData* d = (rendThrData*)data;
+    double x0, y0, x, y, xtmp;
+    int py, px, itr;
+    for(py = 0; py < SCR_HGHT; py++){
+        for(px = 0; px < SCR_WDTH; px++){
+            x0 = map(px, 0, SCR_WDTH, -2.5, 1.0);
+            y0 = map(py, 0, SCR_HGHT, -1.0, 1.0);
+            x = 0.0;
+            y = 0.0;
+            itr = 0;
+            while((x*x + y*y < 2*2) and (itr < MAX_ITER)){
+                xtmp = x*x - y*y + x0;
+                y = 2*x*y + y0;
+                x = xtmp;
+                itr++;
+            }
+            d->img[px][py] = itr;
+        }
+    }
+    pthread_exit(NULL);
 }
 
 int main(){
     pthread_t    thrds[THREADS];
     rendThrData* data;
     SDL_Surface* screen;
+    int i,rc;
 
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_EVERYTHING); 
+    generateColorTable();
     screen = SDL_SetVideoMode(SCR_WDTH, SCR_HGHT, SCR_CD, SDL_SWSURFACE);
     data = new rendThrData[THREADS];
-    for(int i = 0; i < ITERATS; i++){
-        SDL_LockSurface(screen);
-        SDL_BlitSurface(data[i % THREADS].img,
-                        NULL, screen, NULL);
-        SDL_UnlockSurface(screen);
-        SDL_Flip(screen);
+    // initialize threads
+    for(i = 0; i < THREADS; i++){
+        rc = pthread_create(&thrds[i], NULL, renderThread, (void*)&data[i]);
+        if(rc){
+            fprintf(stderr, "Couldn't create thread: %d\n", rc);
+        }
     }
+    for(i = 0; i < FRAMES; i++){
+        pthread_join(thrds[i % THREADS], NULL); // Join current thread
+        SDL_LockSurface(screen);
+        for(int x = 0; x < SCR_WDTH; x++){
+            for(int y = 0; y < SCR_HGHT; y++){
+                put_px(screen, x, y, &colorTable[data[i%THREADS].img[x][y]]);
+            }
+        }
+        SDL_UnlockSurface(screen);
+        if(SDL_Flip(screen) == -1){
+            fprintf(stderr, "SDL_Flip Failed");
+            return 1;
+        }
+        // Recreate the thread 
+        rc = pthread_create(&thrds[i % THREADS], NULL, renderThread, 
+                (void*)&data[i % THREADS]);
+        if(rc){
+            fprintf(stderr, "Couldn't create thread: %d\n", rc);
+        }
+    }
+    for(i = 0; i < THREADS; i++){
+        pthread_join(thrds[i], NULL);
+    }
+    delete[] data;
     SDL_Quit();
 }
